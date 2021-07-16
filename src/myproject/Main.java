6a065954df69fec959214bccb801b0e77a7554fd
naramasato <naramasato@192.168.3.3>
2021-07-16 18:57:01 +0900

    second commit

--- src/myproject/Main.java
+++ src/myproject/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-	System.out.println("Hello World!!");
+		int a;
+		int b = 10;
+		System.out.println(a = b);
 	}
-
 }
