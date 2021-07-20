72f6534eb5918a7e5cedd30a5b348c05fb604ea4
naramasato <naramasato@192.168.3.3>
2021-07-20 09:19:23 +0900

    3-1

--- src/myproject/Main.java
+++ src/myproject/Main.java
@@ -3,25 +3,14 @@ package myproject;
 public class Main {
 
 	public static void main(String[] args) {
-		// 練習問題
-		
-		int x = 5;
-		int y = 10;
-		String ans = "x+yは" + (x + y);
-		System.out.println(ans);
-		
-		//占いゲーム
-		
-		System.out.println("占いの館へようこそ");
-		System.out.println("あなたの名前を入力してください");
-		String name = new java.util.Scanner(System.in).nextLine();
-		System.out.println("あなたの年齢を入力してください");
-		String ageString = new java.util.Scanner(System.in).nextLine();
-		int age = Integer.parseInt(ageString);
-		int fortune = new java.util.Random().nextInt(4);
-		fortune++;
-		System.out.println("占い結果が出ました！");
-		System.out.println(age +"際の" + name + "さん、あなたの運気番号は" + fortune + "です");
-		System.out.println("１：大吉　２：吉　３：中吉　４：凶");
+		//3章　条件分岐
+		//天気による行動の変化
+		boolean tenki = false;
+		if (tenki == true) {
+			System.out.println("洗濯します");
+			System.out.println("散歩にいきます");
+		  } else {
+			System.out.println("DVDを見ます");
+		}
 	}
 }
