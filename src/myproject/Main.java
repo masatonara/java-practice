e84907fa298cf535978edac1d9a29b5233971966
naramasato <naramasato@localhost>
2021-07-21 10:37:44 +0900

    九九の式

--- src/myproject/Main.java
+++ src/myproject/Main.java
@@ -4,23 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		//3章　条件分岐
-		System.out.println("あなたの運勢を占います");
-		int fortune = new java.util.Random().nextInt(5) +1;
-		switch(fortune) {
-		case 1:
-		case 2:
-			System.out.println("いいね！");
-			break;
-		case 3:
-			System.out.println("普通です");
-			break;
-		case 4:
-		case 5:
-			System.out.println("うーん・・・残念");
-		}
-		
-		for (int i = 0; i < 3; i++) {
-			System.out.println("現在" + (i + 1) + "週目⇨");
+		//九九に式
+		for (int i = 1; i < 10; i++) {
+			for (int j = 1; j < 10; j++) {
+				System.out.println(i * j); //掛け算の結果を出力
+				System.out.println("　"); //空白を出力
+			}
+			System.out.println(""); //改行を出力
 		}
 	}
 }
